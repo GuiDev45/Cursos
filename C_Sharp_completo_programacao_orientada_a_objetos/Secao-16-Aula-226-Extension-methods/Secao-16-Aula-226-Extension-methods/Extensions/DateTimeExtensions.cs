01c651ef10b65737a74ddb82b93f2573c1f1c8ea
GuiDev45 <bortolettodev@gmail.com>
2022-05-31 09:19:07 -0300

    Aulas da seção 16

---
+++ C_Sharp_completo_programacao_orientada_a_objetos/Secao-16-Aula-226-Extension-methods/Secao-16-Aula-226-Extension-methods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace System//Pode dizer que essa classe é do System e não precisa usar o caminha do arquivo.
+{
+    static class DateTimeExtensions
+    {
+        //Até a onde eu entendi é uma função static.
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ C_Sharp_completo_programacao_orientada_a_objetos/Secao-16-Aula-226-Extension-methods/Secao-16-Aula-226-Extension-methods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Apenas com o System já é possivel fazer a instância
+
+namespace Secao_16_Aula_226_Extension_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
